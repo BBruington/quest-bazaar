dc94d6207f53103bbd77b93a9140685404facd55
BIBruington <psychological_chemist@hotmail.com>
2023-08-08 04:39:43 -0400

    dnd social media app init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
+import type { AppProps } from "next/app";
+import { ClerkProvider } from '@clerk/nextjs'
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import { api } from "../utils/api";
+import "npm/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({
+  Component,
+  pageProps: { ...pageProps },
+}: AppProps) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
