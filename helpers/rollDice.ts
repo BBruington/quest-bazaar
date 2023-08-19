ad12972e6fdc1cb0b1060c92b955e0a3351ffd07
BIBruington <psychological_chemist@hotmail.com>
2023-08-18 20:41:01 -0500

    die roller start

---
+++ helpers/rollDice.ts
@@ -0,0 +1,14 @@
+const makeARoll = (die: number) => {
+  return Math.floor(Math.random()*(die - 1)) + 1;
+}
+
+export const rollDice = ( numDice: number, dieType: number, bonus = 0) => {
+  const results = {total: 0};
+  for (let i = 1; i < numDice + 1; i++) {
+    const result = makeARoll(dieType);
+    results.total += result;
+  }
+  results.total += bonus;
+  console.log(results)
+  return results;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ClerkProvider } from '@clerk/nextjs'
 import { type AppType } from "next/app";
 import { api } from "../utils/api";
-import "npm/styles/globals.css";
+import "../styles/globals.css";
 
 const MyApp: AppType = ({
   Component,
