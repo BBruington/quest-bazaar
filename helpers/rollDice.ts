7ee0109dc80b70ef8d5b29595c4b6a824e9e9e9c
BIBruington <psychological_chemist@hotmail.com>
2023-08-20 23:16:49 -0400

    .

--- helpers/rollDice.ts
+++ helpers/rollDice.ts
@@ -3,12 +3,12 @@ const makeARoll = (die: number) => {
 }
 
 export const rollDice = ( numDice: number, dieType: number, bonus = 0) => {
-  const results = {total: 0};
+  const results = { total: 0, rolls: [] as number[] };
   for (let i = 1; i < numDice + 1; i++) {
     const result = makeARoll(dieType);
     results.total += result;
+    results.rolls.push(result);
   }
   results.total += bonus;
-  console.log(results)
   return results;
 }
