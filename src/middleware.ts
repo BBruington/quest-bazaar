a7b1700588c45264d8a2c3bfab24c8eeccf994bb
BIBruington <psychological_chemist@hotmail.com>
2023-08-26 16:20:41 -0400

    clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
-
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
-});
-
-// Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({});
+ 
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+ 
