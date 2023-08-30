3cff481c914737669d4e84208781056768a08c2f
BIBruington <psychological_chemist@hotmail.com>
2023-08-30 00:42:22 -0400

    create new user api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,14 +1,13 @@
 import { z } from "zod";
-import { UserSchema } from "types/Types";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  // getAll: publicProcedure.query(({ ctx }) => {
-  //   return ctx.prisma.example.findMany();
-  // }),
 
-  createUser: publicProcedure.input(UserSchema).mutation(async ({ctx, input}) => {
-    const newUser = await ctx.prisma.user.create({ data: input });
+  createUser: publicProcedure.input(z.object({
+    email: z.string(),
+    username: z.string().optional(),
+  })).mutation(async ({ctx, input}) => {
+    const newUser = await ctx.prisma.user.create({ data: {...input} });
 
     return newUser;
   })
