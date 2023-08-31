9398cac1678db8ef7d2f8752740224329a421598
BIBruington <psychological_chemist@hotmail.com>
2023-08-31 19:03:07 -0400

    create user on signup

---
+++ src/pages/api/trpc/createUser.ts
@@ -0,0 +1,43 @@
+import { prisma } from "~/server/db";
+import type { IncomingHttpHeaders } from 'http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { WebhookRequiredHeaders } from 'svix';
+import type { WebhookEvent } from '@clerk/nextjs/server';
+import { Webhook } from 'svix';
+
+const webhookSecret: string  = process.env.WEBHOOK_SECRET!;
+
+export default async function createUser(
+    req: NextApiRequestWithSvixRequiredHeaders,
+    res: NextApiResponse
+  ) {
+  
+    const payload = JSON.stringify(req.body);
+    const headers = req.headers;
+
+    // Create a new Webhook instance with your webhook secret
+    const wh = new Webhook(webhookSecret);
+
+    let evt: WebhookEvent;
+
+    try {
+      // Verify the webhook payload and headers
+      evt = wh.verify(payload, headers) as WebhookEvent;
+    } catch(_) {
+      // If the verification fails, return a 400 error
+      return res.status(400).json({});
+    }
+    const { id } = evt.data;
+
+    const eventType = evt.type;
+    if ( eventType === 'user.created') {
+      // const newUser = await prisma.user.create({ data: {} });
+      console.log("req", payload);
+      console.log("header", headers);
+      console.log("evt data", evt.data);
+    }
+}
+
+type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
