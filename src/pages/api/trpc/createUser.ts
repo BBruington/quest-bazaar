7072f0349be4d1bb4b8ad29ffff541f49160b697
BIBruington <psychological_chemist@hotmail.com>
2023-08-31 19:08:28 -0400

    .

--- src/pages/api/trpc/createUser.ts
+++ src/pages/api/trpc/createUser.ts
@@ -1,43 +1,43 @@
 // import { prisma } from "~/server/db";
-// import type { IncomingHttpHeaders } from 'http';
-// import type { NextApiRequest, NextApiResponse } from 'next';
-// import type { WebhookRequiredHeaders } from 'svix';
-// import type { WebhookEvent } from '@clerk/nextjs/server';
-// import { Webhook } from 'svix';
+import type { IncomingHttpHeaders } from 'http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { WebhookRequiredHeaders } from 'svix';
+import type { WebhookEvent } from '@clerk/nextjs/server';
+import { Webhook } from 'svix';
 
-// const webhookSecret: string  = process.env.WEBHOOK_SECRET!;
+const webhookSecret: string  = process.env.WEBHOOK_SECRET!;
 
-// export default  function createUser(
-//     req: NextApiRequestWithSvixRequiredHeaders,
-//     res: NextApiResponse
-//   ) {
+export default  function createUser(
+    req: NextApiRequestWithSvixRequiredHeaders,
+    res: NextApiResponse
+  ) {
   
-//     const payload = JSON.stringify(req.body);
-//     const headers = req.headers;
+    const payload = JSON.stringify(req.body);
+    const headers = req.headers;
 
-//     // Create a new Webhook instance with your webhook secret
-//     const wh = new Webhook(webhookSecret);
+    // Create a new Webhook instance with your webhook secret
+    const wh = new Webhook(webhookSecret);
 
-//     let evt: WebhookEvent;
+    let evt: WebhookEvent;
 
-//     try {
-//       // Verify the webhook payload and headers
-//       evt = wh.verify(payload, headers) as WebhookEvent;
-//     } catch(_) {
-//       // If the verification fails, return a 400 error
-//       return res.status(400).json({});
-//     }
-//     const { id } = evt.data;
+    try {
+      // Verify the webhook payload and headers
+      evt = wh.verify(payload, headers) as WebhookEvent;
+    } catch(_) {
+      // If the verification fails, return a 400 error
+      return res.status(400).json({});
+    }
+    const { id } = evt.data;
 
-//     const eventType = evt.type;
-//     if ( eventType === 'user.created') {
-//       // const newUser = await prisma.user.create({ data: {} });
-//       console.log("req", payload);
-//       console.log("header", headers);
-//       console.log("evt data", evt.data);
-//     }
-// }
+    const eventType = evt.type;
+    if ( eventType === 'user.created') {
+      // const newUser = await prisma.user.create({ data: {} });
+      console.log("req", payload);
+      console.log("header", headers);
+      console.log("evt data", evt.data);
+    }
+}
 
-// type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-//   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-// };
+type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
