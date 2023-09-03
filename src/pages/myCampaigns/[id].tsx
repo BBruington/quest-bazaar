c545b8a7594690870b4fcfe21e040fe0262e60c0
BIBruington <psychological_chemist@hotmail.com>
2023-09-03 17:30:50 -0400

    campaign page

--- src/pages/myCampaigns/[id].tsx
+++ src/pages/myCampaigns/[id].tsx
@@ -1,27 +1,59 @@
-import { GetStaticProps } from "next";
-import { api } from "~/utils/api";
 import { prisma } from "~/server/db";
+import type {
+  GetStaticProps,
+  GetStaticPaths,
+  InferGetServerSidePropsType
+} from 'next'
 
-export default function Campaign() {
+export default function Campaign({campaignData}: InferGetServerSidePropsType<typeof getStaticProps>) {
+
+  console.log(campaignData.name)
 
   return (
   <>
-    
+    <div>{campaignData.name}</div>
   </>
   )
 }
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const campaignsData =  await prisma.campaign.findMany();
 
   const paths = campaignsData.map((campaign) => ({
     params: {
       id: campaign.id
-    }
+    },
   }))
 
   return({
     paths,
-    fallback: 'blocking',
+    fallback: false,
   })
+}
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  
+  if (!params || typeof params.id !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
+
+  const campaignData = await prisma.campaign.findUnique({
+    where: {
+      id: params.id
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      dmUserId: true,
+    }
+  })
+  console.log(campaignData)
+
+  return {
+    props: {
+      campaignData
+    }
+  }
 }
