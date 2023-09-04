f26c04b661980600b64ba908d673dcc28cd9a626
BIBruington <psychological_chemist@hotmail.com>
2023-09-04 17:38:04 -0400

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  ignoredRoutes: ["/api/routes/webhook"],
-  publicRoutes: ["/api/routes/webhook"],
+  ignoredRoutes: ["routes/webhook"],
+  publicRoutes: ["routes/webhook"],
 });
  
 export const config = {
