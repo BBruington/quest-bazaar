36a0ab579ceab88dd00b2be79b07411a8fedc124
BIBruington <psychological_chemist@hotmail.com>
2023-09-13 20:25:16 -0400

    post image

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function Post({params}: {
   params: { id: string; };
 }) {
 
-  const {data, isLoading: postLoading} = api.queryPost.useQuery({id: params.id})
+  const {data: post, isLoading: postLoading} = api.queryPost.useQuery({id: params.id})
   const {user} = useUser();
   const [postProps, setPostProps] = useState(null)
   const [submitted, setSubmitted] = useState(false);
@@ -25,7 +25,7 @@ export default function Post({params}: {
   // }
 
   if ( postLoading ) return <div>loading...</div>
-  if (!data) return <div>error fetching post</div>
+  if (!post) return <div>error fetching post</div>
 
   if (!user) return null;
 
@@ -33,20 +33,20 @@ export default function Post({params}: {
     <>
       <main>
 
-        <img className="w-full h-40 object-cover" src={`${data.mainImage}`} alt='' />
+        <img className="w-full h-40 object-cover" src={`${post.mainImage ? post.mainImage : 'https://scgovlibrary.librarymarket.com/sites/default/files/2020-12/dndmobile-br-1559158957902.jpg'}`} alt='' />
         <article className="max-w-3xl mx-auto p-5">
 
-          <h1 className="text-3xl mt-10 mb-3">{data.title}</h1>
+          <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
         
-          <h2 className="text-xl font-light text-gray-500 mb-2">{data.description}</h2>
+          <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
 
           <div className="flex items-center space-x-2">
 
-            {/* <img className="h-10 w-10 rounded-full" src={urlFor(data.author.image).url()} alt='' /> */}
+            {/* <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()} alt='' /> */}
           
             <p className="font-extralight text-sm">
-              Blog post by <span className="text-green-600">{data.author}</span> - Published at{" "} 
-            {new Date(data.createdAt).toLocaleString()}</p>
+              By: <span className="text-green-600">{post.author}</span> - Published at{" "} 
+            {new Date(post.createdAt).toLocaleString()}</p>
           
           </div>
 
@@ -55,10 +55,10 @@ export default function Post({params}: {
           </div>
         </article>
 
-        <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+        <hr className="max-w-lg my-5 mx-auto border border-blue-500" />
         
         {submitted ? (
-          <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+          <div className="flex flex-col p-10 my-10 bg-blue-500 text-white max-w-2xl mx-auto">
               <h3 className="text-3xl font-bold">Thank you for your comment</h3>
               <p>Once it has been approved, it will appear below.</p>
           </div>
@@ -69,7 +69,7 @@ export default function Post({params}: {
           onSubmit={() => console.log("hi")}
           className="flex flex-col p-10 my-10 max-w-2xl mx-auto mb-10">
             
-            <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+            {/* <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3> */}
             <h4 className="text-3xl font-bold">Leave a comment below!</h4>
             <hr className="py-3 my-2"></hr>
 
@@ -77,14 +77,14 @@ export default function Post({params}: {
               // {...register("_id")}
               type="hidden"
               name="_id"
-              value={data.id}
+              value={post.id}
             />
 
             <label className="block mb-5">
               <span className="text-gray-700">Name</span> {/*block elements have their own width */}
               <input 
               // {...register("name", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-blue-500 outline-none focus:ring" 
               placeholder="John Smith" type="text" />
             </label>
 
@@ -92,7 +92,7 @@ export default function Post({params}: {
               <span className="text-gray-700">Email</span>
               <input
               // {...register("email", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-blue-500 outline-none focus:ring" 
               placeholder="email@hotmail.com" type="email" />
             </label>
 
@@ -100,7 +100,7 @@ export default function Post({params}: {
               <span className="text-gray-700">Comment</span>
               <textarea 
               // {...register("comment", { required: true })}            
-              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-blue-500 outline-none focus:ring" 
               placeholder="Leave a comment here" rows={8}/>
             </label>
 
@@ -120,7 +120,7 @@ export default function Post({params}: {
 
             <input 
             type="submit" 
-            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" 
+            className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" 
             />
 
           </form>
@@ -133,7 +133,7 @@ export default function Post({params}: {
           <h3 className="text-4xl">Comments</h3>
           <hr className="pb-2" />
 
-          {data.comments[0] ? (post.comments.map((comment) => (
+          {post.comments[0] ? (post.comments.map((comment) => (
             <div key={comment._id}>
               <p>
                 <span className="text-yellow-500">{comment.name}: </span>{comment.comment}
