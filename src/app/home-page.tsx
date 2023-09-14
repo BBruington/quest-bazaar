d49d09d800b9dbbdfd4979e1a2e0d297f3a86880
BIBruington <psychological_chemist@hotmail.com>
2023-09-14 19:17:48 -0400

    shadcn

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -1,8 +1,10 @@
 import Posts from "~/components/posts/page";
+import { Button } from "~/components/ui/button";
 export default function Home() {
 
   return (
     <>
+    <Button variant="destructive">Button</Button>
       <Posts />
     </>
   );
