0d5f25a7512684e33b78b23debfba138499c41b1
BIBruington <psychological_chemist@hotmail.com>
2023-09-24 18:53:53 -0400

    campaign page

--- src/components/campaign.tsx
+++ src/components/campaign.tsx
@@ -5,6 +5,12 @@ import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import type { Campaign } from "@prisma/client";
+
+import { 
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger, } from "./ui/accordion";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -36,29 +42,57 @@ export default function CampaignComponent(props: {campaignData: Campaign}) {
     <div className="flex space-x-3">
       <div>{campaign.name}</div>
       <div>{campaign.description}</div>
-      <AlertDialog>
-        <AlertDialogTrigger>
-          <Button variant="destructive" >Delete Campaign</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your campaign
-              and remove it from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={(e) => {
-              e.preventDefault();
-              mutate({
-                id: campaignData.id
-              })
-            }}>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <div className="w-1/6 mx-2">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Notes</AccordionTrigger>
+            <AccordionContent>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Players</AccordionTrigger>
+            <AccordionContent>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Posts</AccordionTrigger>
+            <AccordionContent>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>Campaign</AccordionTrigger>
+            <AccordionContent>
+
+              <div className="space-y-3">            
+                <AlertDialog>
+                  <AlertDialogTrigger>
+                    <Button variant="destructive" >Delete Campaign</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your campaign
+                        and remove it from our servers.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction onClick={(e) => {
+                        e.preventDefault();
+                        mutate({
+                          id: campaignData.id
+                        })
+                      }}>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
   </div>
   )
 }
