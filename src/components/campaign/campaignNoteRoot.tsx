373a4c74992b2a3967bb2814cdb4c2c33bd3fad9
BIBruington <psychological_chemist@hotmail.com>
2023-09-26 15:58:33 -0400

    cleanup

--- src/components/campaign/campaignNoteRoot.tsx
+++ src/components/campaign/campaignNoteRoot.tsx
@@ -4,9 +4,8 @@ import uuid from 'react-uuid';
 import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
 export default function Notes() {
-  const [emailNotes, setEmailNotes] = useState([]);
+  const [emailNotes, setEmailNotes] = useState(['']);
   const [activeNote, setActiveNote] = useState(false);
   const [addNoteToggle, setAddNoteToggle] = useState(false);
   const [editMode,setEditMode] = useState(false);
@@ -25,87 +24,45 @@ export default function Notes() {
     }]
   }]
 
-  // const deleteNote = async (idToDelete) => {
-  //   if(emailNotes[0]) {
-  //     const updatedNotesArray = [{
-  //       email: emailNotes[0].email,
-  //       id: emailNotes[0].id,
-  //       notes:
-  //       emailNotes[0].notes.filter((note) => {return note.id !== idToDelete})
-  //     }]
-  //       setEmailNotes(updatedNotesArray)
-  //       const noteDoc = doc(db, "user notes", emailNotes[0].id)
-  //       await updateDoc(noteDoc, updatedNotesArray[0])
-  //   }
-  // }
+  const deleteNote = () => {
 
-  // const addNote = async () => {
+    return
+  }
 
-  //   const newNote = {
-  //     title: "Untitled Note",
-  //     body: "",
-  //     id: uuid(),
-  //     lastModified: {
-  //       seconds: Date.now()/1000,
-  //       milliseconds: Date.now()
-  //     }
-  //   };
-  //   const addedNote = emailNotes;
-  //   addedNote[0].notes.unshift(newNote)
+  const addNote = () => {
 
-  //   setEmailNotes(addedNote)
-  //   setAddNoteToggle(!addNoteToggle)
+    return
+   }
+
+  const updateNote = () => {
+
+    return
+  }
+
+  const getActiveNote = () => {
+
+    return false
+  }
+  return (
+    <>
     
-  //   const noteDoc = doc(db, "user notes", emailNotes[0].id)
-  //   await updateDoc(noteDoc, addedNote[0])
-  //  }
-
-  // const updateNote = async (updatedNote) => {
-  //   if(emailNotes[0]) {
-  //     const updatedNotesArray = [{
-  //       email: emailNotes[0].email,
-  //       id: emailNotes[0].id,
-  //       notes:
-  //       emailNotes[0].notes.map((note) => {
-  //       if(note.id === activeNote) {
-  //         return updatedNote;
-  //       }
-  //       return note;
-  //     })}]
-  //     setEmailNotes(updatedNotesArray)
-  //     const noteDoc = doc(db, "user notes", emailNotes[0].id)
-  //     await updateDoc(noteDoc, updatedNotesArray[0])
-  //   }
-  // }
-
-  // const getActiveNote = () => {
-  //   if(emailNotes[0]){
-  //     const activeNoteListener = emailNotes[0].notes.find((note) => note.id === activeNote);
-  //     return activeNoteListener
-  //   }
-  // }
-  // if (error) return <div>failed to load</div>
-  // if (isLoading) return <div>loading...</div>
-  // return (
-  //   <>
-    
-  //     <div className="flex justify-start">
-  //         <CampaignNotesSideBar 
-  //         emailNotes={emailNotes} 
-  //         addNote={addNote} 
-  //         deleteNote={deleteNote} 
-  //         activeNote={activeNote}
-  //         setActiveNote={setActiveNote}
-  //         editMode={editMode}
-  //         setEditMode={setEditMode}
-  //         />
-  //         <CampaignNotesMain 
-  //         activeNote={getActiveNote()}
-  //         updateNote={updateNote}
-  //         editMode={editMode}          
-  //         />
-  //     </div>
-  //   </>
-  // )
+      <div className="flex justify-start">
+          <CampaignNotesSideBar 
+          emailNotes={emailNotes} 
+          addNote={addNote} 
+          deleteNote={deleteNote} 
+          activeNote={activeNote}
+          setActiveNote={setActiveNote}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          />
+          <CampaignNotesMain 
+          activeNote={activeNote}
+          updateNote={updateNote}
+          editMode={editMode}          
+          />
+      </div>
+    </>
+  )
 }
 
