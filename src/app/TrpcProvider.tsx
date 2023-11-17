957eeaa7d05b2c7db0feee0b91226930de6e1ea1
BIBruington <psychological_chemist@hotmail.com>
2023-11-16 21:58:58 -0500

    ENV

--- src/app/TrpcProvider.tsx
+++ src/app/TrpcProvider.tsx
@@ -1,26 +1,26 @@
-"use client"
+"use client";
 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
-import {httpBatchLink} from "@trpc/client"
-import {useState} from "react"
-import {api} from "../utils/trpc"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
+import { api } from "../utils/trpc";
 
-export const TrpcProvider: React.FC<{children: React.ReactNode}> = p => {
-    const [queryClient] = useState(() => new QueryClient())
-    const [trpcClient] = useState(() =>
-        api.createClient({
-            links: [
-                httpBatchLink({
-                    url: `${process.env.BASE_URL}/api/trpc`
-                })
-            ],
-        })
-    )
-    return (
-        <api.Provider client={trpcClient} queryClient={queryClient}>
-            <QueryClientProvider client={queryClient}>
-                {p.children}
-             </QueryClientProvider>
-        </api.Provider>
-    )
-}
+export const TrpcProvider: React.FC<{ children: React.ReactNode }> = (p) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    api.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/trpc`,
+        }),
+      ],
+    })
+  );
+  return (
+    <api.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {p.children}
+      </QueryClientProvider>
+    </api.Provider>
+  );
+};
