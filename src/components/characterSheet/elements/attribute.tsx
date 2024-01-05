e1de017152d2cdd31f9bdd49d2761d716a479013
BIBruington <psychological_chemist@hotmail.com>
2024-01-04 21:56:47 -0600

    .

--- src/components/characterSheet/elements/attribute.tsx
+++ src/components/characterSheet/elements/attribute.tsx
@@ -2,16 +2,25 @@ import { Label } from "@radix-ui/react-label";
 import { useFormContext, type SubmitHandler } from "react-hook-form";
 import type { CharacterForm } from "~/components/characterSheet/characterTypes";
 
-const CharacterAttribute = (props: { name: string, modName: string, label: string, score: number | null, saveCharacter: SubmitHandler<CharacterForm> }) => {
-  
-  const {name, modName, label,score, saveCharacter} = props
-  const { register, handleSubmit } = useFormContext(); 
+const CharacterAttribute = (props: {
+  name: string;
+  modName: string;
+  label: string;
+  score: number | null;
+  saveCharacter: SubmitHandler<CharacterForm>;
+}) => {
+  const { name, modName, label, score, saveCharacter } = props;
+  const { register, handleSubmit } = useFormContext();
 
   return (
     <li className="flex flex-col items-center justify-center rounded-xl border-2 border-black bg-white">
       <div className="score flex flex-col">
         <Label
-          className={`text-center ${label === "Constitution" || "Intelligence" ? 'text-[9px]' : 'text-xs'} font-bold`}
+          className={`text-center ${
+            label === "Constitution" || "Intelligence"
+              ? "text-[9px]"
+              : "text-xs"
+          } font-bold`}
           htmlFor="strengthscore"
         >
           {label}
@@ -30,11 +39,7 @@ const CharacterAttribute = (props: { name: string, modName: string, label: strin
       </div>
       <div className="modifier flex justify-center">
         <input
-          value={`+${
-            score
-              ? Math.floor((score - 10) / 2)
-              : 0
-          }`}
+          value={`+${score ? Math.floor((score - 10) / 2) : 0}`}
           className="m-[-1px] w-10 rounded-xl border-2 border-black text-center"
           id={modName}
           name={modName}
