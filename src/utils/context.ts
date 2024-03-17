d8407b057eedda3575cefb9ed580b1a3259084b2
BIBruington <psychological_chemist@hotmail.com>
2024-03-16 20:09:38 -0400

    update context

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -10,4 +10,4 @@ const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({ adapter })
