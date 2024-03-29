c4cd16c2578082fb9dff1949896096cb48441a8d
BIBruington <psychological_chemist@hotmail.com>
2024-03-28 19:31:57 -0500

    remove types no longer used

--- src/app/types/Campaign.ts
+++
@@ -1,9 +0,0 @@
-// export interface Campaign {
-//     id: string;
-//     name: string;
-//     description: string;
-//     password: string | null;
-//     dmUserId: string | null;
-//     createdAt: Date | string;
-//     updatedAt: Date | string;
-// }
