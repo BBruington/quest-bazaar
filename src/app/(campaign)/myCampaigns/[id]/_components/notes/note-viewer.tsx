198e5c867f0fbafb1cc875dee6076585b1cc5633
BIBruington <psychological_chemist@hotmail.com>
2024-04-05 17:51:46 -0500

    save not on stop typing

--- src/app/(campaign)/myCampaigns/[id]/_components/notes/note-viewer.tsx
+++ src/app/(campaign)/myCampaigns/[id]/_components/notes/note-viewer.tsx
@@ -3,7 +3,6 @@ import type { Campaign } from "../types";
 import { Textarea } from "~/components/ui/textarea";
 import { useEffect, useState } from "react";
 import type { CampaignNote } from "./types";
-import { Button } from "~/components/ui/button";
 import { upsertCampaignNote } from "../../actions";
 
 const NoteViewer = (props: {
@@ -14,10 +13,16 @@ const NoteViewer = (props: {
 }) => {
   const { note, campaignData, privateNotes, userId } = props;
   const [campaignNote, setCampaigNote] = useState(note);
-  const [editMode, setEditmode] = useState(false);
   useEffect(() => {
     setCampaigNote(note);
   }, [note]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleSaveNote()
+    }, 1000); 
+    return () => clearTimeout(timeoutId);
+  }, [campaignNote]);
 
   const handleSaveNote = async () => {
     if (
@@ -45,47 +50,25 @@ const NoteViewer = (props: {
   };
   return (
     <div className="flex w-5/6 flex-col items-center text-white">
-      <div className="mb-2 flex items-center gap-5">
-        <Button className="h-7" onClick={() => setEditmode(!editMode)}>
-          Edit
-        </Button>
-        <Button className="h-7" onClick={() => handleSaveNote()}>
-          Save
-        </Button>
-      </div>
       {campaignNote && (
-        <>
-          <div className="flex h-full w-5/6 flex-col">
-            <h2 className="flex h-1/6 w-full items-center justify-center bg-accent-foreground text-5xl">
-              {campaignNote.title}
-            </h2>
-            <p className="mt-5 flex h-5/6 w-full bg-accent-foreground p-5">
-              {campaignNote.content}
-            </p>
-          </div>
-          {editMode ? (
-            <div className="flex h-full w-5/6 flex-col">
-              <Textarea
-                className="flex h-1/6 w-full items-center justify-center bg-accent-foreground text-5xl"
-                id="title"
-                value={campaignNote.title}
-                onChange={(e) => editField("title", e.target.value)}
-                placeholder="Title"
-                onBlur={() => handleSaveNote()}
-              ></Textarea>
-              <Textarea
-                className="mt-5 flex h-5/6 w-full bg-accent-foreground p-5"
-                id="content"
-                value={campaignNote.content}
-                onChange={(e) => editField("content", e.target.value)}
-                placeholder="Write your note here"
-                onBlur={() => handleSaveNote()}
-              ></Textarea>
-            </div>
-          ) : (
-            <></>
-          )}
-        </>
+        <div className="flex h-full w-5/6 flex-col">
+          <Textarea
+            className="flex h-1/6 w-full items-center justify-center bg-accent-foreground text-5xl"
+            id="title"
+            value={campaignNote.title}
+            onChange={(e) => editField("title", e.target.value)}
+            placeholder="Title"
+            onBlur={() => handleSaveNote()}
+          ></Textarea>
+          <Textarea
+            className="mt-5 flex h-5/6 w-full bg-accent-foreground p-5"
+            id="content"
+            value={campaignNote.content}
+            onChange={(e) => editField("content", e.target.value)}
+            placeholder="Write your note here"
+            onBlur={() => handleSaveNote()}
+          ></Textarea>
+        </div>
       )}
     </div>
   );
